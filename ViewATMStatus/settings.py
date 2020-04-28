542cc9cbf60630e91b133f7a353c4c9ade4d9963
Suraj Acharya <suraj.acharya62@gmail.com>
2020-04-28 14:30:03 +0545

    add Staticfiles

--- ViewATMStatus/settings.py
+++ ViewATMStatus/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ViewATMStatus.urls'
@@ -120,7 +121,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
